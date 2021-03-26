62176641d94550ae76a4631fbf0dfd16316c60d1
juanmahecha9 <juanmahecha.dc@gmail.com>
2021-03-26 15:50:07 -0400

    modificacion juego mario

--- D.mario/mario.js
+++ D.mario/mario.js
@@ -1,32 +1,52 @@
 let screen0 = {
   tablero: { x: 0, y: 0, w: 40, h: 40, color: "blue" },
   superficies: [],
+  enemigosSyD: [
+    {
+      x: 20,
+      y: 28,
+      w: 1,
+      h: 1,
+      color: "white",
+      vx: 0,
+    },
+  ],
+  time: 0,
 };
-screen0.superficies.push(...newSurfaceX(39, 0, 40)); // superficie total
-screen0.superficies.push(...newSurfaceX(29, 15, 22));
-screen0.superficies.push(...newSurfaceX(25, 10, 18));
-screen0.superficies.push(...newSurfaceX(23, 20, 25));
-screen0.superficies.push(...newSurfaceX(20, 10, 18));
-screen0.superficies.push(...newSurfaceX(17, 20, 25));
-screen0.superficies.push(...newSurfaceX(15, 25, 30));
+screen0.superficies.push(...newSurfaceX(39, 0, 20)); //suelo
+screen0.superficies.push(...newSurfaceX(39, 27, 40)); //suelo
 
-screen0.superficies.push(...newSurfaceY(30, 13, 40));
-screen0.superficies.push(...newSurfaceT(18, 36, 40));
-screen0.superficies.push(...newSurfaceT(22, 33, 40));
+screen0.superficies.push(...newSurfaceX(29, 15, 22)); //plataformas flotantes
+screen0.superficies.push(...newSurfaceX(26, 10, 18)); //plataformas flotantes
+screen0.superficies.push(...newSurfaceX(24, 20, 25)); //plataformas flotantes
+screen0.superficies.push(...newSurfaceX(21, 23, 28)); //plataformas flotantes
+screen0.superficies.push(...newSurfaceX(19, 17, 22)); //plataformas flotantes
+screen0.superficies.push(...newSurfaceX(15, 22, 25)); //plataformas flotantes
+screen0.superficies.push(...newSurfaceX(13, 26, 29)); //plataformas flotantes
+
+screen0.superficies.push(...newSurfaceY(30, 13, 40)); //plataforma
+
+screen0.superficies.push(...newSurfaceT(18, 36, 40)); //tubos
+screen0.superficies.push(...newSurfaceT(22, 33, 40)); //tubos
 
 let screen1 = {
   tablero: { x: 0, y: 0, w: 40, h: 40, color: "#05A8FF" },
   superficies: [],
+  enemigosSyD: [],
+  time: 0,
 };
 screen1.superficies.push(...newSurfaceX(39, 0, 10));
 screen1.superficies.push(...newSurfaceX(39, 0, 10));
 screen1.superficies.push(...newSurfaceX(39, 15, 25));
 screen1.superficies.push(...newSurfaceX(39, 30, 40));
-screen1.superficies.push(...newSurfaceT(21, 33, 40));
+
+screen1.superficies.push(...newSurfaceT(21, 36, 40));
 
 let screen2 = {
   tablero: { x: 0, y: 0, w: 40, h: 40, color: "#2305FF" },
   superficies: [],
+  enemigosSyD: [],
+  time: 0,
 };
 //
 screen2.superficies.push(...newSurfaceX(39, 0, 3));
@@ -42,7 +62,7 @@ screen2.superficies.push(...newSurfaceY(38, 34, 40));
 
 let state = {
   screen: [screen0, screen1, screen2],
-  user: { x: 0, y: 15, w: 1, h: 1, color: " black" },
+  user: { x: 0, y: 33, w: 1, h: 1, color: " black" },
   nubes: [
     { x: 1, y: 1, w: 2, h: 2, color: "white" },
     { x: 5, y: 2, w: 2, h: 2, color: "white" },
@@ -54,9 +74,12 @@ let state = {
     { x: 35, y: 2, w: 2, h: 2, color: "white" },
   ],
 };
+//crear enemigos
 
 //Variable que define la pantalla
 let numberScreen = 0;
+//conatdor de vidas
+let vidas = 4;
 
 //Contexto y funcion RECT
 ctx = canvas.getContext("2d");
@@ -83,6 +106,10 @@ function draw() {
   );
   //dibujo de la superficie
   for (pixel of dibujo.superficies) {
+    Rect(pixel.x, pixel.y, pixel.w, pixel.h, pixel.color);
+  }
+  //dibujo enemigos
+  for (pixel of dibujo.enemigosSyD) {
     Rect(pixel.x, pixel.y, pixel.w, pixel.h, pixel.color);
   }
   //dibujo de las nubes
@@ -143,8 +170,12 @@ update = function () {
       state.nubes[nube].x = 40;
     }
   }
-  updateUser(state.user);
+
+  updateUser(state.user); //update usuario
+  updateEnemigosSyD(state.screen[numberScreen].enemigosSyD, state.user); //update enemiegos SysD
   colide();
+  escribir("cont1", vidas); // contador de las vidas
+  pauseAnimation(vidas);
   draw();
 };
 colide = function () {};
@@ -160,7 +191,7 @@ function updateUser(user) {
   }
   if (user.y >= 40) {
     user.x = 0;
-    user.y = 20;
+    user.y = 33;
   }
   // esta en la plataforma
   let isOnSurface = isXYOnSurface(user.x, user.y); //superficie
@@ -173,31 +204,74 @@ function updateUser(user) {
       state.user.y2 = 4;
     }
   }
+  //
   new_x = state.user.x;
   new_y = state.user.y;
   //mover el usuario
   if (state.key == "ArrowLeft") {
     new_x = state.user.x - 1;
-
   } else if (state.key == "ArrowRight") {
     new_x = state.user.x + 1;
   } // eventopara saltar
-  else if (state.key == "ArrowUp") {
+  else if (state.key == " ") {
     if (isOnSurface) {
       new_y = state.user.y - 4;
     }
-  } 
-  if(isXYGoodPosition(new_x, new_y)){
+  }
+  if (isXYGoodPosition(new_x, new_y)) {
     state.user.x = new_x;
     state.user.y = new_y;
- }
+  }
   state.key = null;
+  // matar al enemigo
+  let pixel = state.screen[numberScreen].enemigosSyD;
+  for (i = 0; i <= pixel - 1; i++) {
+    if (user.x == pixel[i].x && user.y - 1 == pixel[i].y) {
+      pixel.splice(i,1)
+    }
+  }
 }
 
-function isXYGoodPosition(x, y){
+function updateEnemigosSyD(enemigo, user) {
+  // actualizacion de los movimientos
+  for (let i = 0; i <= enemigo.length - 1; i++) {
+    //movimiento de los enemigos
+    let time = state.screen[numberScreen].time++;
+    if (time > 10) {
+      enemigo[i].x = enemigo[i].x + enemigo[i].vx;
+      time = 0;
+    }
+    let isOnSurface = isXYOnSurface(enemigo[i].x, enemigo[i].y); //esta en la superficie
+    if (!isOnSurface) {
+      //cae si no esta en una superficie
+      enemigo[i].y2 = !!enemigo[i].y2 - 1;
+      if (enemigo[i].y2 < 0) {
+        enemigo[i].y++;
+        enemigo[i].y2 = 4;
+      }
+    }
+
+    //ir a buscar al enemigo
+    if (enemigo[i].x >= user.x) {
+      enemigo[i].vx = -1;
+    } else if (enemigo[i].x <= user.x) {
+      enemigo[i].vx = 1;
+    }
+
+    // el el enemigo toca al usuario
+    /* if (enemigo[i].x == user.x && enemigo[i].y == user.y) {
+      alert("Crash...!");
+      user.x = 0;
+      user.y = 30;
+      //restar vidas
+      vidas = vidas - 1;
+    } */
+  }
+}
+
+function isXYGoodPosition(x, y) {
   // chequea que la posicion no sea una superficie (o un enemigo)
   return !isXYOnArray(x, y, state.screen[numberScreen].superficies);
-
 }
 
 function isXYOnSurface(x, y) {
@@ -216,6 +290,17 @@ function isXYOnArray(x, y, array) {
 //
 function isXYOnBarril(x, y) {
   return isXYOnArray(x, y, state.barriles);
+}
+//ecribir
+function escribir(id, x) {
+  document.getElementById(id).innerHTML = x;
+}
+
+//Pausar animacion
+function pauseAnimation(x) {
+  if (x <= 0) {
+    clearInterval(intervalo);
+  }
 }
 function ciclo() {
   update();
